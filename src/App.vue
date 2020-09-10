bde61c4b90213a0bacab99f2c3b208e7a9514c62
Jesse Väärälä <jesse.vaarala@gmail.com>
2020-09-10 20:44:21 +0300

    example

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Fillarit :results="this.results"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Fillarit from './components/Fillarit.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            Fillarit
+        },
+        methods: {
+            fetchApi() {
+                const myHeaders = new Headers();
+                myHeaders.append("Content-Type", "application/json");
+
+                const graphql = JSON.stringify({
+                    query: "  query {\n  bikeRentalStations {\n    stationId\n    name\n    bikesAvailable\n    spacesAvailable\n    lat\n    lon\n    allowDropoff\n  }\n  }",
+                    variables: {}
+                });
+                const requestOptions = {
+                    method: 'POST',
+                    headers: myHeaders,
+                    body: graphql,
+                    redirect: 'follow'
+                };
+
+                fetch("https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql", requestOptions)
+                    .then(response => response.text())
+                    .then(result => this.results = JSON.parse(result))
+                    .catch(error => console.log('error', error));
+            }
+        },
+        mounted() {
+            this.stationData = this.fetchApi();
+        },
+        data: function () {
+            return {
+                results: JSON
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
